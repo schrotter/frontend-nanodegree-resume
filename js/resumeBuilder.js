b2015e9ac8f857cb0e73d77788881a85c169ecb5
schrotter <schrotter@sonour.com>
2017-02-17 16:41:55 +0100

    Append lesson

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,28 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var email = "cameron@udacity.com";
+var newEmail = email.replace("udacity", "gmail");
+
+console.log(email);
+console.log(newEmail);
+
+var awesomeThoughts = "My name is Jim and I am AWESOME";
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+$("#main").append(funThoughts);
+
+ /*
+----------------------------
+ */
+
+var name = "Jimssss";
+var role = "Expert";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedRole);
+
