706e7fd78ff56d37f9267cddd82535bbac8b499f
schrotter <schrotter@sonour.com>
2017-02-22 14:37:07 +0100

    Slice

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -26,3 +26,21 @@ var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedName);
 $("#header").prepend(formattedRole);
 
+ /*
+----------------------------
+ */
+
+var s = "audacity";
+
+var udacityizer = function(s) {  
+    // Right now, the variable s === "audacity"
+    // Manipulate s to make it equal to "Udacity"
+    // Your code goes here!
+    str1 = "Uda";
+    str2 = s.slice(4, 8);
+    
+    return str1 + str2;
+};
+
+// Did your code work? The line below will tell you!
+console.log(udacityizer(s));
